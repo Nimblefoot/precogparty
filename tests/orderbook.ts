2ce88ef3944f5d067799fbd6de36a21489f9ccc5
Sapphire Alysa Star <sapphire.alysa.star@gmail.com>
2022-05-02 15:52:14 -0400

    split up orderbook tests

--- tests/orderbook.ts
+++ tests/orderbook.ts
@@ -35,212 +35,220 @@ describe("orderbook", async () => {
   const admin = Keypair.generate();
   const user = Keypair.generate();
 
-  describe("end-to-end", async () => {
-    it("works", async () => {
-      await program.provider.connection.confirmTransaction(
-        await program.provider.connection.requestAirdrop(
-          admin.publicKey,
-          10000000000
-        ),
-        "finalized"
-      );
-      await program.provider.connection.confirmTransaction(
-        await program.provider.connection.requestAirdrop(
-          user.publicKey,
-          1000000000
-        ),
-        "finalized"
-      );
+  // All PDAs set in `before` block
+  let userAccountAddress: PublicKey;
+  let orderbookInfo: PublicKey;
+  let firstPage: PublicKey;
+  let currencyVault: PublicKey;
+  let tokenVault: PublicKey;
+  let currencyMint: PublicKey;
+  let tokenMint: PublicKey;
 
-      console.log("create a user account and check initialize");
-      await program.methods
-        .createUserAccount()
-        .accounts({
-          user: user.publicKey,
-        })
-        .signers([user])
-        .rpc();
-
-      const [userAccountAddress] = await PublicKey.findProgramAddress(
-        [utf8.encode("user-account"), user.publicKey.toBuffer()],
-        program.programId
-      );
-      let userAccount = await program.account.userAccount.fetch(
-        userAccountAddress
-      );
-      assert.equal(
-        userAccount.user.toString(),
-        user.publicKey.toString(),
-        "user should match the creator"
-      );
-      assert.equal(
-        // @ts-ignore
-        userAccount.orders.length,
-        0,
-        "initially orders should be empty"
-      );
-
-      console.log("creates two mints and initialize an orderbook");
-      const [orderbookInfo] = await PublicKey.findProgramAddress(
-        [utf8.encode("test"), utf8.encode("orderbook-info")],
-        program.programId
-      );
-
-      const currencyMint = await createMint(
-        program.provider.connection,
-        admin,
+  before(async () => {
+    /** SETUP */
+    await program.provider.connection.confirmTransaction(
+      await program.provider.connection.requestAirdrop(
         admin.publicKey,
-        null,
-        6
-      );
+        10000000000
+      ),
+      "finalized"
+    );
+    await program.provider.connection.confirmTransaction(
+      await program.provider.connection.requestAirdrop(
+        user.publicKey,
+        1000000000
+      ),
+      "finalized"
+    );
 
-      const currencyVault = await getAssociatedTokenAddress(
-        currencyMint,
-        orderbookInfo,
-        true
-      );
+    currencyMint = await createMint(
+      program.provider.connection,
+      admin,
+      admin.publicKey,
+      null,
+      6
+    );
 
-      const tokenMint = await createMint(
-        program.provider.connection,
-        admin,
-        admin.publicKey,
-        null,
-        6
-      );
+    tokenMint = await createMint(
+      program.provider.connection,
+      admin,
+      admin.publicKey,
+      null,
+      6
+    );
 
-      const tokenVault = await getAssociatedTokenAddress(
-        tokenMint,
-        orderbookInfo,
-        true
-      );
+    /** PDAs */
+    [userAccountAddress] = await PublicKey.findProgramAddress(
+      [utf8.encode("user-account"), user.publicKey.toBuffer()],
+      program.programId
+    );
 
-      const [firstPage] = await PublicKey.findProgramAddress(
-        [
-          utf8.encode("test"),
-          utf8.encode("page"),
-          new anchor.BN(0).toArrayLike(Buffer, "le", 4),
-        ],
-        program.programId
-      );
+    [orderbookInfo] = await PublicKey.findProgramAddress(
+      [utf8.encode("test"), utf8.encode("orderbook-info")],
+      program.programId
+    );
 
-      await program.methods
-        .initializeOrderbook("test")
-        .accounts({
-          admin: program.provider.wallet.publicKey,
-          currencyMint,
-          currencyVault,
-          tokenMint,
-          tokenVault,
-          orderbookInfo,
-          firstPage,
-        })
-        .rpc();
+    [firstPage] = await PublicKey.findProgramAddress(
+      [
+        utf8.encode("test"),
+        utf8.encode("page"),
+        new anchor.BN(0).toArrayLike(Buffer, "le", 4),
+      ],
+      program.programId
+    );
 
-      let orderbookData = await program.account.orderbookInfo.fetch(
-        orderbookInfo
-      );
+    currencyVault = await getAssociatedTokenAddress(
+      currencyMint,
+      orderbookInfo,
+      true
+    );
+    tokenVault = await getAssociatedTokenAddress(
+      tokenMint,
+      orderbookInfo,
+      true
+    );
+  });
 
-      assert.equal(
-        orderbookData.currencyMint.toString(),
-        currencyMint.toString(),
-        "currency mints should match"
-      );
-      assert.equal(
-        orderbookData.tokenMint.toString(),
-        tokenMint.toString(),
-        "token mints should match"
-      );
-
-      let mintAccount = await getMint(
-        program.provider.connection,
-        currencyMint
-      );
-
-      let ata = await createAssociatedTokenAccount(
-        program.provider.connection, // connection
-        user, // fee payer
-        currencyMint, // mint
-        user.publicKey // owner,
-      );
-
-      let txhash = await mintToChecked(
-        program.provider.connection, // connection
-        user, // fee payer
-        currencyMint, // mint
-        ata, // receiver (sholud be a token account)
-        admin, // mint authority
-        2e8, // amount. if your decimals is 8, you mint 10^8 for 1 token.
-        6 // decimals
-      );
-
-      let tokenAmount =
-        await program.provider.connection.getTokenAccountBalance(ata);
-
-      console.log("lets place some order!");
-      const size = 10;
-      const mockData = [...Array(size).keys()].map((i) => ({
+  it("creates a user account", async () => {
+    await program.methods
+      .createUserAccount()
+      .accounts({
         user: user.publicKey,
-        size: new anchor.BN((1e8 / 100) * (i + 1)),
-        buy: true,
-        price: new anchor.BN(1),
-      }));
+      })
+      .signers([user])
+      .rpc();
 
-      for (let i = 0; i < size; i++) {
-        const keysAndData = await getKeysAndData(program, "test");
+    let userAccount = await program.account.userAccount.fetch(
+      userAccountAddress
+    );
+    assert.equal(
+      userAccount.user.toString(),
+      user.publicKey.toString(),
+      "user should match the creator"
+    );
+    assert.equal(
+      // @ts-ignore
+      userAccount.orders.length,
+      0,
+      "initially orders should be empty"
+    );
+  });
 
-        await program.methods
-          .placeOrder("test", mockData[i])
-          .accounts({
-            user: user.publicKey,
-            userAta: ata,
-            vault: currencyVault,
-            orderbookInfo,
-            currentPage: keysAndData.pageKeys[keysAndData.info.lastPage],
-            userAccount: userAccountAddress,
-          })
-          .signers([user])
-          .rpc();
-      }
+  it("initializes an orderbook", async () => {
+    await program.methods
+      .initializeOrderbook("test")
+      .accounts({
+        admin: program.provider.wallet.publicKey,
+        currencyMint,
+        currencyVault,
+        tokenMint,
+        tokenVault,
+        orderbookInfo,
+        firstPage,
+      })
+      .rpc();
 
-      const vaultBalance =
-        await program.provider.connection.getTokenAccountBalance(currencyVault);
-      assert.equal(
-        vaultBalance.value.amount,
-        "55000000",
-        "Vault Balance should match sum of orders." // sum 1 to 10 = 55
-      );
+    let orderbookData = await program.account.orderbookInfo.fetch(
+      orderbookInfo
+    );
 
+    assert.equal(
+      orderbookData.currencyMint.toString(),
+      currencyMint.toString(),
+      "currency mints should match"
+    );
+    assert.equal(
+      orderbookData.tokenMint.toString(),
+      tokenMint.toString(),
+      "token mints should match"
+    );
+  });
+
+  it("places 10 orders", async () => {
+    //let mintAccount = await getMint(program.provider.connection, currencyMint);
+
+    let ata = await createAssociatedTokenAccount(
+      program.provider.connection, // connection
+      user, // fee payer
+      currencyMint, // mint
+      user.publicKey // owner,
+    );
+
+    let txhash = await mintToChecked(
+      program.provider.connection, // connection
+      user, // fee payer
+      currencyMint, // mint
+      ata, // receiver (sholud be a token account)
+      admin, // mint authority
+      2e8, // amount. if your decimals is 8, you mint 10^8 for 1 token.
+      6 // decimals
+    );
+
+    const size = 10;
+    const mockData = [...Array(size).keys()].map((i) => ({
+      user: user.publicKey,
+      size: new anchor.BN((1e8 / 100) * (i + 1)),
+      buy: true,
+      price: new anchor.BN(1),
+    }));
+
+    for (let i = 0; i < size; i++) {
       const keysAndData = await getKeysAndData(program, "test");
-      assert.equal(keysAndData.info.length, 10, "correct orderbook length");
-      assert.equal(
-        // @ts-ignore
-        keysAndData.lastPage.list.length,
-        1,
-        "correct length of final chunk"
-      );
-      userAccount = await program.account.userAccount.fetch(userAccountAddress);
-      const seventhOrder = userAccount.orders[6];
-      assert.equal(
-        seventhOrder.size.toString(),
-        "7000000",
-        "correct size for order"
-      );
-      assert.equal(seventhOrder.price, 1, "correct price for order");
 
-      const firstOrder = mockData[0];
       await program.methods
-        .cancelOrder("test", firstOrder, 0, 0)
+        .placeOrder("test", mockData[i])
         .accounts({
           user: user.publicKey,
-          userAccount: userAccountAddress,
           userAta: ata,
           vault: currencyVault,
           orderbookInfo,
-          orderPage: firstPage,
-          lastPage: keysAndData.pageKeys[keysAndData.info.lastPage],
+          currentPage: keysAndData.pageKeys[keysAndData.info.lastPage],
+          userAccount: userAccountAddress,
         })
         .signers([user])
         .rpc();
-    });
+    }
+
+    const vaultBalance =
+      await program.provider.connection.getTokenAccountBalance(currencyVault);
+    assert.equal(
+      vaultBalance.value.amount,
+      "55000000",
+      "Vault Balance should match sum of orders." // sum 1 to 10 = 55
+    );
+
+    const keysAndData = await getKeysAndData(program, "test");
+    assert.equal(keysAndData.info.length, 10, "correct orderbook length");
+    assert.equal(
+      // @ts-ignore
+      keysAndData.lastPage.list.length,
+      1,
+      "correct length of final chunk"
+    );
+    const userAccount = await program.account.userAccount.fetch(
+      userAccountAddress
+    );
+    const seventhOrder = userAccount.orders[6];
+    assert.equal(
+      seventhOrder.size.toString(),
+      "7000000",
+      "correct size for order"
+    );
+    assert.equal(seventhOrder.price, 1, "correct price for order");
+
+    const firstOrder = mockData[0];
+    await program.methods
+      .cancelOrder("test", firstOrder, 0, 0)
+      .accounts({
+        user: user.publicKey,
+        userAccount: userAccountAddress,
+        userAta: ata,
+        vault: currencyVault,
+        orderbookInfo,
+        orderPage: firstPage,
+        lastPage: keysAndData.pageKeys[keysAndData.info.lastPage],
+      })
+      .signers([user])
+      .rpc();
   });
 });
