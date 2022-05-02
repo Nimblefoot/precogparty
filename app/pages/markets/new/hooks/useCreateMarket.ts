c739c668cc47c9180355be2d444410fb00ddb2f5
agrippa kellum <ask252@cornell.edu>
2022-05-02 16:06:18 -0400

    various tailwind template crap

--- app/pages/markets/new/hooks/useCreateMarket.ts
+++ app/pages/markets/new/hooks/useCreateMarket.ts
@@ -1,3 +1,11 @@
-const useCreateMarket = () => {};
+import { useCallback } from "react";
+
+const useCreateMarket = () => {
+  const createMarket = useCallback(() => {
+    // upload description to arweave if it is > char limit
+    // instructions: create orderbooks
+    // instruction: create market
+  }, []);
+};
 
 export default useCreateMarket;
