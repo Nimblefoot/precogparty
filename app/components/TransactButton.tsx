5526773734da2cb14c478097a1300542aaea04eb
agrippa kellum <ask252@cornell.edu>
2022-05-04 16:55:03 -0400

    i made a cute button

--- app/components/TransactButton.tsx
+++ app/components/TransactButton.tsx
@@ -1,4 +1,7 @@
-export type Status = "initial" | "sending" | "confirming";
+import { useState } from "react";
+import ReactCanvasConfetti from "react-canvas-confetti";
+
+export type Status = "initial" | "signing" | "sending" | "confirming" | "done";
 type Props = {
   verb: string;
   status: Status;
@@ -8,22 +11,46 @@ type Props = {
 export default function TransactButton({ verb, status, disabled }: Props) {
   return (
     <>
-      <button
-        disabled={disabled}
-        className={`ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md 
+      <div>
+        <ReactCanvasConfetti
+          style={{
+            position: "fixed",
+            pointerEvents: "none",
+            width: "100%",
+            height: "100%",
+            top: 0,
+            left: 0,
+          }}
+          // origin = {x, y}
+          width={1000}
+          height={1000}
+          fire={status === "done"}
+          disableForReducedMotion
+        />
+        <button
+          disabled={disabled}
+          className={`ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md 
         text-white 
           focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
-          ${status === "initial" ? " bg-indigo-600 hover:bg-indigo-700" : ""}
+          ${
+            status === "initial" || status === "done"
+              ? " bg-indigo-600 hover:bg-indigo-700"
+              : ""
+          }
+          ${status === "signing" ? " bg-gray-400  animate-pulse " : ""}
           ${status === "sending" ? " bg-gray-400  animate-pulse " : ""}
-          ${status === "confirming" ? "bg-piss animate-rainbow " : ""}
+          ${status === "confirming" ? "animate-rainbow " : ""}
         `}
-      >
-        {status === "initial"
-          ? verb
-          : status === "sending"
-          ? "Sending..."
-          : "Confirming..."}
-      </button>
+        >
+          {status === "initial" || status === "done"
+            ? verb
+            : status === "sending"
+            ? "Sending..."
+            : status === "signing"
+            ? "Signing..."
+            : "Confirming..."}
+        </button>
+      </div>
     </>
   );
 }
