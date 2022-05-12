d95fb0608257e777aa7a77be140b302a562cde81
agrippa kellum <ask252@cornell.edu>
2022-05-12 19:02:10 -0400

    positions page

--- app/pages/positions/Positions.tsx
+++ app/pages/positions/Positions.tsx
@@ -1,7 +1,27 @@
+import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/outline"
 import { PublicKey } from "@solana/web3.js"
+import Link from "next/link"
 import { useMarket, useMarkets } from "pages/markets/Market/hooks/marketQueries"
 import { useAllTokenAccounts } from "pages/tokenAccountQuery"
 import React, { useMemo } from "react"
+
+const YesBadge = () => (
+  <>
+    <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-lime-100 text-lime-800">
+      <CheckCircleIcon className="h-4 w-4 -ml-1 mr-1" />
+      YES
+    </span>
+  </>
+)
+
+const NoBadge = () => (
+  <>
+    <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-rose-100 text-rose-800">
+      <XCircleIcon className="h-4 w-4 -ml-1 mr-1" />
+      NO
+    </span>
+  </>
+)
 
 const usePositions = () => {
   const accounts = useAllTokenAccounts()
@@ -13,37 +33,28 @@ const usePositions = () => {
     if (!markets.data) return undefined
     if (!accounts.data) return undefined
 
-    return accounts.data.value
-      .map((account) => {
-        const mint = new PublicKey(account.account.data.parsed.info.mint)
+    const accountsSimplified = accounts.data.value.map((account) => ({
+      mint: new PublicKey(account.account.data.parsed.info.mint),
+      uiAmount: account.account.data.parsed.info.tokenAmount.uiAmount,
+    }))
 
-        const [yesMarket] = markets.data.filter((market) =>
-          market.account.yesMint.equals(mint)
+    const nonzero = accountsSimplified.filter(({ uiAmount }) => uiAmount > 0)
+
+    return markets.data
+      .map((market) => {
+        const [yesAccount] = nonzero.filter(({ mint }) =>
+          mint.equals(market.account.yesMint)
+        )
+        const [noAccount] = nonzero.filter(({ mint }) =>
+          mint.equals(market.account.noMint)
         )
 
-        if (yesMarket) {
+        if (yesAccount || noAccount)
           return {
-            mint,
-            uiAmount: account.account.data.parsed.info.tokenAmount.uiAmount,
-            marketAddress: yesMarket.publicKey,
-            position: "yes",
+            marketAddress: market.publicKey,
+            yesAccount,
+            noAccount,
           } as const
-        }
-
-        const [noMarket] = markets.data.filter((market) =>
-          market.account.noMint.equals(mint)
-        )
-        if (noMarket) {
-          return {
-            mint,
-            uiAmount: account.account.data.parsed.info.tokenAmount
-              .uiAmount as number,
-            marketAddress: noMarket.publicKey,
-            position: "no",
-          } as const
-        }
-
-        return undefined
       })
       .filter((x): x is typeof x & {} => x !== undefined)
   }, [accounts.data, markets.data])
@@ -58,7 +69,7 @@ const Positions = ({}) => {
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
-          <h1 className="text-xl font-semibold text-gray-900">Positions</h1>
+          <h1 className="text-xl font-semibold text-gray-900">Tokens</h1>
           <p className="mt-2 text-sm text-gray-700">
             A list of all the users in your account including their name, title,
             email and role.
@@ -76,13 +87,13 @@ const Positions = ({}) => {
                       scope="col"
                       className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
                     >
-                      Name
+                      Market
                     </th>
                     <th
                       scope="col"
                       className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                     >
-                      Position
+                      Balance
                     </th>
 
                     <th
@@ -95,7 +106,10 @@ const Positions = ({}) => {
                 </thead>
                 <tbody className="divide-y divide-gray-200 bg-white">
                   {positions?.map((position) => (
-                    <Position key={position.mint.toString()} {...position} />
+                    <Position
+                      key={position.marketAddress.toString()}
+                      {...position}
+                    />
                   ))}
                 </tbody>
               </table>
@@ -111,24 +125,38 @@ export default Positions
 
 function Position({
   marketAddress,
-  mint,
-  uiAmount,
-  position,
+  yesAccount,
+  noAccount,
 }: NonNullable<ReturnType<typeof usePositions>>[number]) {
   const market = useMarket(marketAddress)
 
   return (
     <tr>
-      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-        {market.data?.name}
+      <td
+        className={`
+          whitespace-nowrap py-4 pl-4 pr-3 text-lg font-medium sm:pl-6
+          ${market.data?.resolution === 0 ? "text-gray-900" : "text-gray-500"}
+        `}
+      >
+        <Link href={`/markets?m=${market.data?.name}`} passHref>
+          <a>
+            {market.data?.name} {market.data?.resolution === 1 && <YesBadge />}
+            {market.data?.resolution === 2 && <NoBadge />}
+          </a>
+        </Link>
       </td>
       <td
         className={`
-        whitespace-nowrap px-3 py-4 text-sm
-        ${position === "yes" ? "text-lime-600" : "text-rose-600"}
+        whitespace-nowrap px-3 py-4 text-sm 
       `}
       >
-        {uiAmount} {position}
+        {yesAccount && (
+          <span className="text-lime-700">${yesAccount.uiAmount} YES</span>
+        )}
+        {yesAccount && noAccount && <>{",  "}</>}
+        {noAccount && (
+          <span className="text-rose-700">${noAccount.uiAmount} NO</span>
+        )}
       </td>
 
       <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6"></td>
