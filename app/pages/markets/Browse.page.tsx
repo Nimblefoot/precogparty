70aef8f1187192553a85a231d7f8955884367c60
agrippa kellum <ask252@cornell.edu>
2022-05-13 00:41:21 -0400

    no auto complete

--- app/pages/markets/Browse.page.tsx
+++ app/pages/markets/Browse.page.tsx
@@ -38,7 +38,8 @@ export default function Browse() {
                 name="search"
                 className="block w-full bg-white border border-gray-300 rounded-md py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 placeholder="Search"
-                type="search"
+                type="text"
+                autoComplete="off"
                 value={searchString}
                 onChange={(e) => setSearchString(e.target.value)}
               />
