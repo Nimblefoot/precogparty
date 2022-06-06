6ac9947e58745062f18a2599b8d2e64d4dd6434e
agrippa kellum <ask252@cornell.edu>
2022-06-06 15:38:22 -0400

    confirmation alerts are rpc aware

--- app/hooks/useConfirmationAlert.tsx
+++ app/hooks/useConfirmationAlert.tsx
@@ -5,6 +5,16 @@ import { useCallback } from "react"
 const useConfirmationAlert = () => {
   const { enqueueSnackbar } = useSnackbar()
 
+  const getTxUrl = (tx: string) => {
+    if (process.env.NEXT_PUBLIC_RPC?.includes("devnet")) {
+      return `https://solscan.io/tx/${tx}?cluster=devnet`
+    } else if (process.env.NEXT_PUBLIC_RPC?.includes("localhost")) {
+      return `https://explorer.solana.com/tx/${tx}?cluster=custom&customUrl=http%3A%2F%2Flocalhost%3A8899`
+    } else {
+      return `https://solscan.io/tx/${tx}`
+    }
+  }
+
   const success = useCallback(
     (msg: string, tx: string) => {
       enqueueSnackbar(msg, {
@@ -13,11 +23,7 @@ const useConfirmationAlert = () => {
         //content: <div>poopy</div>,
         action: (
           <div className="ml-4">
-            <a
-              href={`https://solscan.io/tx/${tx}?cluster=${process.env.NEXT_PUBLIC_CLUSTER}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href={getTxUrl(tx)} target="_blank" rel="noopener noreferrer">
               <button
                 type="button"
                 className="rounded-md hover:bg-opacity-20 hover:bg-white inline-flex items-center p-2 border border-white text-white shadow-sm text-xs font-medium"
