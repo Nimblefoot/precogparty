1c1551581a6c59d0663c5ceee87d45fb757b48df
agrippa kellum <ask252@cornell.edu>
2022-07-06 21:39:37 -0400

    no weird search bar padding

--- app/src/pages/markets/browse.page.tsx
+++ app/src/pages/markets/browse.page.tsx
@@ -20,8 +20,8 @@ export default function Browse() {
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-      <div className="min-w-0 flex-1 md:px-8 lg:px-0 xl:col-span-6">
-        <div className="flex items-center px-6 py-4 md:max-w-3xl md:mx-auto lg:max-w-none lg:mx-0 xl:px-0">
+      <div className="min-w-0 flex-1">
+        <div className="flex items-center py-4">
           <div className="w-full">
             <label htmlFor="search" className="sr-only">
               Search
