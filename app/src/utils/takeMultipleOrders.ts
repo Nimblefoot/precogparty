1feb46a0209fbdde5a86af18cdb72d624e9d90e4
Sapphire Alysa Star <sapphire.alysa.star@gmail.com>
2022-07-23 21:37:00 -0400

    basic setup. test passes

--- app/src/utils/takeMultipleOrders.ts
+++ app/src/utils/takeMultipleOrders.ts
@@ -1,7 +1,11 @@
-import { OrderFields } from "@/generated/syrup/types"
 import { PublicKey, Keypair } from "@solana/web3.js"
 import BN from "bn.js"
+import { utf8 } from "@project-serum/anchor/dist/cjs/utils/bytes"
 import * as anchor from "@project-serum/anchor"
+
+import { PROGRAM_ID as SYRUP_ID } from "@/generated/syrup/programId"
+import { useSyrup } from "src/hooks/useProgram"
+import { OrderFields } from "@/generated/syrup/types"
 
 export type BN_ = InstanceType<typeof BN>
 
@@ -29,15 +33,77 @@ export interface RemainingAccount {
   isWritable: boolean
 }
 
-export const takeMultipleOrders = function (
+export const takeMultipleOrders = async (
   orderParams: Array<takeOrderData>,
   length: number,
-  pageSize: number
-): [Array<OrderMetadata>, Array<RemainingAccount>] {
+  pageSize: number,
+  marketAddress: PublicKey,
+  mint: PublicKey
+): Promise<[Array<OrderMetadata>, Array<RemainingAccount>]> => {
   let orders: Array<OrderMetadata>
   let accounts: Array<RemainingAccount>
   orders = []
   accounts = []
+
+  for (let { order, size, pageNumber, index } of orderParams) {
+    const [orderbookInfo] = await PublicKey.findProgramAddress(
+      [marketAddress.toBuffer(), utf8.encode("orderbook-info")],
+      SYRUP_ID
+    )
+
+    const takerSendingAta = await getAssociatedTokenAddress(
+      offeringApples ? noMint : yesMint,
+      publicKey
+    )
+
+    // TODO get or create
+    const takerReceivingAta = await getAssociatedTokenAddress(
+      offeringApples ? yesMint : noMint,
+      publicKey
+    )
+
+    const offererReceivingAta = await getAssociatedTokenAddress(
+      offeringApples ? noMint : yesMint,
+      order.user
+    )
+
+    const [offererUserAccount] = await PublicKey.findProgramAddress(
+      [utf8.encode("user-account"), order.user.toBuffer()],
+      SYRUP_ID
+    )
+
+    const [orderPage] = await PublicKey.findProgramAddress(
+      [
+        marketAddress.toBuffer(),
+        utf8.encode("page"),
+        new BN(pageNumber).toArrayLike(Buffer, "le", 4),
+      ],
+      SYRUP_ID
+    )
+
+    const lastPageIndex = Math.floor(
+      (orderbookQuery.data.info.length - 1) / ORDERBOOK_PAGE_MAX_LENGTH
+    )
+
+    const [lastPage] = await PublicKey.findProgramAddress(
+      [
+        marketAddress.toBuffer(),
+        utf8.encode("page"),
+        new BN(lastPageIndex).toArrayLike(Buffer, "le", 4),
+      ],
+      SYRUP_ID
+    )
+
+    const [takerTradeLog] = await PublicKey.findProgramAddress(
+      [publicKey.toBuffer(), utf8.encode("trade-log")],
+      SYRUP_ID
+    )
+
+    const [offererTradeLog] = await PublicKey.findProgramAddress(
+      [order.user.toBuffer(), utf8.encode("trade-log")],
+      SYRUP_ID
+    )
+  }
 
   return [[], []]
 }
